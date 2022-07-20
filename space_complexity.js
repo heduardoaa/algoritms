6ee47de199b84f078c5c22e545e742ff3683ea1e
Héctor Altamirano <healtamiranoa@localhost.localdomain>
2022-07-20 13:31:16 +0000

    first commit

---
+++ space_complexity.js
@@ -0,0 +1,27 @@
+const sum = (n) => {
+    // console.log(n);
+    if(n <= 0) {
+        return 0;
+    }
+    const x = n + sum(n - 1);
+    console.log("sum: " + x); 
+    return x;
+};
+
+sum(999);
+
+const pairSumSequence = (n) => {
+    let sum = 0;
+    for(let i = 0; i < n; i++) {
+        sum += pairSum(i, i + 1);
+    }
+    console.log("result: " + sum);
+    return sum;
+}
+ const pairSum = (a, b) => {
+    const result = a + b;
+    console.log("pairing sum: " + result);
+    return result;
+ }
+
+ pairSumSequence(3);
