8ee93a42d118dd2a50a62202b57d36b690cb03ec
Héctor E. Altamirano Aguilar <altamiranohe@globalhitss.com>
2022-07-21 21:00:16 -0500

    Automated Repo Update

--- book/p40_space_complexity.ts
+++ book/p40_space_complexity.ts
@@ -1,26 +1,29 @@
+
+// Space complexity is a parallel concept to time complexity
+
+
+// Code like this would take 0(n) time and 0(n^2) space.
 const sum = (n: any) => {
     if(n <= 0) {
         return 0;
     }
     const x: any = n + sum(n - 1);
-    console.log("sum: " + x); 
     return x;
 };
 
-sum(999);
+console.log("result sum() : " + sum(2));
 
+// Just because you have n calls total doesn't mean it takes 0(n) space. 
 const pairSumSequence = (n: number) => {
-    let sum = 0;
+    let sum = 0; 
     for(let i = 0; i < n; i++) {
         sum += pairSum(i, i + 1);
     }
-    console.log("result: " + sum);
     return sum;
 }
  const pairSum = (a: number, b: number) => {
     const result = a + b;
-    console.log("pairing sum: " + result);
     return result;
  }
 
- pairSumSequence(3);
+ console.log("result pairSumSequence() : " + pairSumSequence(2));
