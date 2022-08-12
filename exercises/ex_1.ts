b240c0c80d2b37ced198646cffb897c9b4b754fb
Héctor Altamirano <altamiranohe@globalhitss.com>
2022-08-11 22:01:02 -0500

    Automated Repo Update

--- exercises/ex_1.ts
+++ exercises/ex_1.ts
@@ -1,7 +1,7 @@
-// TODO: function for get how many pairs of number have on number's array
+// TODO: function for get how many pairs of numbers and number have on number's array
+// same result, different structure in the result
 
 // solution 1
-const n = 9;
 const arr = [ 10, 20, 20, 10, 10, 30, 50, 10, 20, 10, 50, 50, 90, 90, 100, 80, 6, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 5, 4, 6, 3, 2, 1, 5, 9, 5, 2, 6, 6, 5, 4, 1, 2, 3, 6, 9, 8, 7, 1, 1, 2, 3, 2, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 ];
 
 const getPairsfromArray = ( arr: any ): any => {
