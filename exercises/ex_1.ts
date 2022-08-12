d4eacadb95f2464cdca73bc3d588d0e85cc171ed
Héctor Altamirano <altamiranohe@globalhitss.com>
2022-08-11 21:15:59 -0500

    Automated Repo Update

--- exercises/ex_1.ts
+++ exercises/ex_1.ts
@@ -1,5 +1,6 @@
 // TODO: function for get how many pairs of number have on number's array
 
+// solution 1
 const n = 9;
 const arr = [ 10, 20, 20, 10, 10, 30, 50, 10, 20, 10, 50, 50, 90, 90, 100, 80, 6, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 5, 4, 6, 3, 2, 1, 5, 9, 5, 2, 6, 6, 5, 4, 1, 2, 3, 6, 9, 8, 7, 1, 1, 2, 3, 2, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 ];
 
@@ -15,10 +16,10 @@ const getPairsfromArray = ( arr: any ): any => {
         if ( arr[ j ] === prime[ j ] ) {
             const number: string = prime[ j ].toString();
             if ( dataObj && Object.keys( dataObj ).indexOf( number ) > -1 ) {
-                dataObj[ number ].quantity += 1;
+                dataObj[ number ].count += 1;
             } else {
                 dataObj[ number ] = {};
-                dataObj[ number ].quantity = 1;
+                dataObj[ number ].count = 1;
             }
         }
     }
@@ -27,7 +28,7 @@ const getPairsfromArray = ( arr: any ): any => {
         if ( arr[ j ] === prime[ j ] ) {
             const number: string = prime[ j ].toString();
             if ( JsonResult && Object.keys( JsonResult ).indexOf( number ) !== -1 ) {
-                JsonResult[ number ].numberPairs = Number.isInteger( JsonResult[ number ].numberPairs ) ? ( JsonResult[ number ].quantity / 2 ) : Math.round( JsonResult[ number ].quantity / 2 ) - 1;
+                JsonResult[ number ].numberPairs = Number.isInteger( JsonResult[ number ].numberPairs ) ? ( JsonResult[ number ].count / 2 ) : Math.round( JsonResult[ number ].count / 2 ) - 1;
             }
         }
     }
@@ -35,9 +36,21 @@ const getPairsfromArray = ( arr: any ): any => {
 }
 
 const result = getPairsfromArray( arr );
-console.log( "arr" );
-console.log( arr.sort() );
 console.info( result );
 
+// solution 2
+
+const resultX2 = [...new Set(arr)].map(num => {
+    const count: number = (arr.filter(n => n === num).length);
+    let pairs: number = count;
+    pairs = Number.isInteger(pairs / 2) ? pairs / 2 : Math.round(pairs / 2) -1;
+
+    return {
+        element: num,
+        count: count,
+        numberPairs: pairs
+    };
+});
+console.info(resultX2);
 
 
