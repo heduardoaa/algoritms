99d3a087f9737193cf5c7268ee02ffa4efe0afc4
Héctor Altamirano <altamiranohe@globalhitss.com>
2022-08-11 20:11:29 -0500

    Automated Repo Update

--- exercises/ex_2.ts
+++ exercises/ex_2.ts
@@ -1,12 +1,30 @@
 // TODO: function to reverse a string
+
+const wordToReverse: string = "Hello World";
+// solution 1
 const reverseStr = (str: string) => {
-    let txt = "";    
+    let txt = "";
     for(let i = str.length -1; i >= 0; i--) {
         txt += str[i];
     }
     return txt;
 };
 
-const txtReverse = reverseStr("hello world");
-console.info(txtReverse)
+const txtReverse = reverseStr(wordToReverse);
+console.info(txtReverse);
+
+// solution 2
+
+const txtReversex2 = wordToReverse.split("").reverse().join("");
+console.info(txtReversex2);
+
+// solution 3
+
+let txtReversex3 = "";
+
+for(let letter of wordToReverse) {
+    letter += txtReversex3
+    txtReversex3 = letter
+    }
+console.log(txtReversex3);
 
