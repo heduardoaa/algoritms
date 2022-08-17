189e4add61098061317daa827f958518244dbbf9
Héctor Altamirano <altamiranohe@globalhitss.com>
2022-08-16 22:13:59 -0500

    Automated Repo Update

--- exercises/ex_6.ts
+++ exercises/ex_6.ts
@@ -1,11 +1,28 @@
 // create an Array separate by spaces
 
-const words = "lundi mardi    mercredi  jeudi    vendredi    samedi dimanche";
+const words = " lundi mardi    mercredi  jeudi    vendredi    samedi dimanche";
 
 // solution 1
 
-let result: any = words.replace(/\s+/g, " ").split(" ");
+let result: any = words.replace(/\s+/g, " ").trim().split(" ");
 
 console.info(result);
 
 // solution 2
+
+let result2: any = [];
+let wordTmp = "";
+for (let letter of words) {
+    wordTmp += ((letter === " ") ? "" : letter);
+
+    if (wordTmp !== "" && letter === " ") {
+        result2[result2.length] = wordTmp;
+        wordTmp = "";
+    }
+}
+
+if(wordTmp !== "") {
+    result2[result2.length] = wordTmp;
+}
+
+console.info(result2)
