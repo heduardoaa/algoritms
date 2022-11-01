9d15408a514551975452461f6e54dd3df2b9fb81
Héctor Altamirano <heduardoaa@outlook.com>
2022-11-01 17:02:12 -0600

    Automated Repo Update

---
+++ exercises/ex_8.ts
@@ -0,0 +1,31 @@
+/*
+Write  a program that permit show numbers from 1 to 100
+replacing multiples of 3 by "fizz", multiples of 5 for "buzz"
+and multiples of both (3 and 5) "fizzbuzz"
+*/
+
+function showNumber() {
+    let number = [];
+    for(let i = 1; i<=100; i++) {
+        if (i % 3) {
+            number.push(i);
+        } else {
+            number.push("fizz");
+        }
+
+        if (number.length % 5) {
+            // nothing to do
+        } else {
+            if (number[number.length - 1] === "fizz") {
+                number[number.length - 1] = 'fizzbuzz';
+            } else {
+                number[number.length - 1] = "buzz";
+            }
+        }
+    }
+    return number;
+}
+
+
+
+console.log(showNumber());
