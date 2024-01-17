4efc287c5b3f128cf8bf1cd2304d9e8ced215068
Héctor Altamirano <heduardoaa@outlook.com>
2024-01-16 19:26:02 -0600

    Automated Repo Update

--- exercism/test/ex4_test.ts
+++ exercism/test/ex4_test.ts
@@ -3,23 +3,18 @@ import * as assert from "assert";
 
 describe('Resistor Colors', () => {
     it('Orange and orange and black', () => {
-        // @ts-ignore
         assert(decodedResistorValue(['orange', 'orange', 'black']) === '33 ohms');
     })
     it('Blue and grey and brown', () => {
-        // @ts-ignore
         assert(decodedResistorValue(['blue', 'grey', 'brown']) === '680 ohms');
     })
     it('Red and black and red', () => {
-        // @ts-ignore
         assert(decodedResistorValue(['red', 'black', 'red']) === '2 kiloohms');
     })
     it('Green and brown and orange', () => {
-        // @ts-ignore
         assert(decodedResistorValue(['green', 'brown', 'orange']) === '51 kiloohms');
     })
     it('Yellow and violet and yellow', () => {
-        // @ts-ignore
         assert(decodedResistorValue(['yellow', 'violet', 'yellow']) === '470 kiloohms');
     })
 })
